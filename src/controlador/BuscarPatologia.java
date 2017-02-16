fba36cd0a091a4c52335a1cc9aa972fe3ef35f18
iulianang <iulian.anghel19@gmail.com>
2017-02-16 16:53:39 +0100

    Actualizacion de jsp

--- src/controlador/BuscarPatologia.java
+++ src/controlador/BuscarPatologia.java
@@ -32,9 +32,60 @@ public class BuscarPatologia extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 		PatologiaService ps = new PatologiaService();
+		long time_start, time_end;
+		time_start = System.currentTimeMillis();
+		String str_id = request.getParameter("id");
+		int id = Integer.parseInt(str_id);
+		
 		PatologiaDTO patoDto = ps.buscarPatologiaPorId(1);
 		PrintWriter pw = response.getWriter().append(patoDto.getDescripcion());
+		
+		time_end = System.currentTimeMillis();
+		System.out.println("the task has taken "+ ( time_end - time_start ) +" milliseconds");
 	
+		
+		request.setAttribute("patologia", patoDto);
+		
+		switch (id) {
+		case 1:
+			request.getRequestDispatcher(".//html//queratocono.jsp").forward(request, response);
+			
+			break;
+		case 2:
+			request.getRequestDispatcher(".//html//glaucoma.jsp").forward(request, response);
+			
+			break;
+			
+		case 4:
+			request.getRequestDispatcher(".//html//catarata.jsp").forward(request, response);
+			break;
+		
+		case 5:
+			
+			request.getRequestDispatcher(".//html//conjuntivitis.jsp").forward(request, response);
+			
+			break;
+			
+		case 6: 
+			request.getRequestDispatcher(".//html//desprendimiento.jsp").forward(request, response);
+			break;
+			
+		case 7:
+			request.getRequestDispatcher(".//html//acromatopsia.jsp").forward(request, response);
+			break;
+			
+		case 8:
+			request.getRequestDispatcher(".//html//estrabismo.jsp").forward(request, response);
+			break;
+		
+		case 9: 
+			request.getRequestDispatcher(".//html//presbicia.jsp").forward(request, response);
+			break;
+						
+		default:
+			request.getRequestDispatcher(".//html//patologias.html").forward(request, response);
+			break;
+		}
 	}
 
 	/**
