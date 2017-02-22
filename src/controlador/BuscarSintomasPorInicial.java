a9f64d8de072fc3af3b7888612d523726319852b
iulianang <iulian.anghel19@gmail.com>
2017-02-22 21:04:33 +0100

    v4

--- src/controlador/BuscarSintomasPorInicial.java
+++ src/controlador/BuscarSintomasPorInicial.java
@@ -2,7 +2,9 @@ package controlador;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -11,7 +13,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import DTO.PatologiaDTO;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 import DTO.SintomasDTO;
 import servicios.SintomasService;
 
@@ -21,43 +25,54 @@ import servicios.SintomasService;
 @WebServlet("/BuscarSintomasPorInicial")
 public class BuscarSintomasPorInicial extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public BuscarSintomasPorInicial() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		SintomasService ss = new SintomasService();
-		
-		
-		try {
-			String valorSintoma = request.getParameter("sintomaBuscado");
-			List<SintomasDTO> lista_sintomaDTO = ss.buscarSintomasPorInicial(valorSintoma);
-			
-			
-			for(SintomasDTO sintoma: lista_sintomaDTO)
-			{
-				valorSintoma = sintoma.getDescripcion();
-				System.out.println(valorSintoma);
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		//lista_sintomas = (ArrayList<String>) sintomas_service.buscarSintomaPorInicial(valorSintoma);
-		
+	public BuscarSintomasPorInicial() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// response.getWriter().append("Served at:
+		// ").append(request.getContextPath());
+		String inicial = request.getParameter("intro");
+		SintomasService ss = new SintomasService();
+		List<SintomasDTO> lista_sintomas = null;
+		try {
+			lista_sintomas = ss.buscarSintomasPorInicial(inicial);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		/*
+		 * for (SintomaDTO sintoma : lista_sintomas) {
+		 * System.out.println(sintoma); }
+		 */
+		Gson gson = new Gson();
+		Type tipoListaSintomas = new TypeToken<List<SintomasDTO>>() {
+		}.getType();
+		String s = gson.toJson(lista_sintomas, tipoListaSintomas);
+
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(s);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
